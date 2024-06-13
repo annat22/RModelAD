979a0f8709d904a173e65fc285ebf20404c88742
annat22 <annat22@gmail.com>
2024-06-13 16:38:52 -0400

    added gen_ind_meta_id

--- gen_ind_meta_id.R
+++ gen_ind_meta_id.R
@@ -11,7 +11,7 @@ suppressPackageStartupMessages({
 source("https://raw.github.com/TheJacksonLaboratory/ClimbR/master/climbGET.R")
 synLogin(silent=TRUE)
 
-gen_ind_meta <- function(climbID) {
+gen_ind_meta_id <- function(climbID) {
   
   # load template from synapse
   temp <- read_xlsx(synGet("syn21084071")$path, sheet = 1)
